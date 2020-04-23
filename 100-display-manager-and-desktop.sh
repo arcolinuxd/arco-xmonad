07a777557e76c442b8154b10a8dcedfe40a33b1a
Erik Dubois <erik.dubois@gmail.com>
2020-04-23 08:44:42 +0200

    migration

---
+++ 100-display-manager-and-desktop.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+#set -e
+###############################################################################
+# Author	:	Erik Dubois
+# Website	:	https://www.erikdubois.be
+# Website	:	https://www.arcolinux.info
+# Website	:	https://www.arcolinux.com
+# Website	:	https://www.arcolinuxd.com
+# Website	:	https://www.arcolinuxb.com
+# Website	:	https://www.arcolinuxiso.com
+# Website	:	https://www.arcolinuxforum.com
+###############################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+###############################################################################
+
+
+###############################################################################
+#
+#   DECLARATION OF FUNCTIONS
+#
+###############################################################################
+
+
+func_install() {
+	if pacman -Qi $1 &> /dev/null; then
+		tput setaf 2
+  		echo "###############################################################################"
+  		echo "################## The package "$1" is already installed"
+      	echo "###############################################################################"
+      	echo
+		tput sgr0
+	else
+    	tput setaf 3
+    	echo "###############################################################################"
+    	echo "##################  Installing package "  $1
+    	echo "###############################################################################"
+    	echo
+    	tput sgr0
+    	sudo pacman -S --noconfirm --needed $1
+    fi
+}
+
+###############################################################################
+echo "Installation of the core software"
+###############################################################################
+
+list=(
+lightdm
+arcolinux-lightdm-gtk-greeter
+arcolinux-lightdm-gtk-greeter-settings
+arcolinux-wallpapers-git
+xmonad
+xmobar
+xmonad-contrib
+haskell-dbus
+xmonad-utils
+xmonad-log
+checkupdates-aur
+perl-checkupdates-aur
+arcolinux-xmonad-git
+arcolinux-config-xmonad-git
+arcolinux-dconf-xmonad-git
+arcolinux-polybar-git
+polybar
+)
+
+count=0
+
+for name in "${list[@]}" ; do
+	count=$[count+1]
+	tput setaf 3;echo "Installing package nr.  "$count " " $name;tput sgr0;
+	func_install $name
+done
+
+###############################################################################
+
+tput setaf 6;echo "################################################################"
+echo "Copying all files and folders from /etc/skel to ~"
+echo "################################################################"
+echo;tput sgr0
+cp -rT /etc/skel ~
+
+tput setaf 5;echo "################################################################"
+echo "Enabling lightdm as display manager"
+echo "################################################################"
+echo;tput sgr0
+sudo systemctl enable lightdm.service -f
+
+tput setaf 7;echo "################################################################"
+echo "You now have a very minimal functional desktop"
+echo "################################################################"
+echo;tput sgr0
+
+tput setaf 11;
+echo "################################################################"
+echo "Reboot your system"
+echo "################################################################"
+echo;tput sgr0
